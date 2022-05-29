11dbfbf0ce5da55d6435686ae60c6f487376c953
Jiuk Choi <jiukchoi@algotradingsysteminc.com>
2022-05-29 01:20:47 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,8 +3,16 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-const handleHome = () => console.log("Somebody is trying to go home.");
+const handleHome = (req,res) => {
+    return res.send("I still exist.");
+};
+
+const handleLogin = (req,res) => {
+    return res.send ("Login here");
+};
+
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} --Wetube server--`);
 
 app.get("/",handleHome);
+app.get("/login", handleLogin);
 app.listen(PORT, handleListening);
